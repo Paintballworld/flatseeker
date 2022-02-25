068aa4d0302d2bdf930a6704d6e2b3172ef96808
Yerlan <Yakzhanov@gmail.com>
2022-02-25 07:52:13 +0100

    Initial commit

---
+++ src/main/java/com/yakzhanov/flatseeker/FlatseekerApplication.java
@@ -0,0 +1,13 @@
+package com.yakzhanov.flatseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatseekerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlatseekerApplication.class, args);
+    }
+
+}
